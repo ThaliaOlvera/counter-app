7f8ff4cc3049525d365a0d8b072f7e5978d55dd9
ThaliaOlvera <resets>
2023-09-01 17:57:55 -0500

    Button fixed

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+// set initial count
+let count = 0;
+
+//select all values and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
